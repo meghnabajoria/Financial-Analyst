ca0699f6fe15c88bd57ba3bccabff551b081a3d3
meghnabajoria <meghnabajoria8@gmail.com>
2024-03-15 14:23:35 -0700

    added code for gemini

--- app.py
+++ app.py
@@ -3,17 +3,22 @@ from flask import Flask, request, render_template
 from urllib.parse import urlencode
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import openai
-import time
+import google.generativeai as genai
 
+import time
+genai.configure(api_key="AIzaSyD2ut1rrUIzbOSFuW7g-0PT6MGIwjcFAXM")
 app = Flask(__name__)
 
 
 def configure_chrome_options():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('log-level=3')
+    # chrome_options.add_argument('--ignore-certificate-errors')
+    # chrome_options.add_argument('--ignore-ssl-errors')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
     return chrome_options
 
 
@@ -36,6 +41,7 @@ def get_content(link):
         soup2 = BeautifulSoup(page_source, 'html.parser')
         paragraphs = soup2.find_all('p')
         text_content = [p.get_text() for p in paragraphs]
+        print(text_content)
         return text_content
 
     except Exception as e:
@@ -54,15 +60,10 @@ def index():
     if request.method == 'POST':
         context = ""
         question = request.form['question']
-        api_key = request.form['api_key']
-
-        if not question or not api_key:
-            return render_template('error.html', message="Invalid request. Please provide both question and API key.")
-
-        openai.api_key = api_key
 
         search_params = {'searchTerm': question}
         url = f'https://www.kiplinger.com/search?{urlencode(search_params)}'
+        print(url)
 
         try:
             driver = create_webdriver()
@@ -73,16 +74,17 @@ def index():
             soup = BeautifulSoup(page_source, 'html.parser')
             results = soup.find_all('li', class_='listing__item listing__item--alternate')
 
-            for item in results[:1]:
+            for item in results[:3]:
                 link_element = item.find('a', class_='listing__link')
                 if link_element:
                     link = link_element.get('href')
+                    print("=============", link)
                     context_in_one_list = get_content(link)
                     context += ' '.join(context_in_one_list)
                 else:
                     print("Link not found.")
         except Exception as main_error:
-            return render_template('error.html', message=f"Error during main processing: {main_error}")
+            return render_template(message=f"Error during main processing: {main_error}")
 
         finally:
             try:
@@ -91,21 +93,14 @@ def index():
                 print(f"Error while quitting the driver: {quit_error}")
 
         prompt = f"{question}\n\nInput:\n{context}"
-        prompt = " ".join(prompt.split()[:3800])
+        # prompt = " ".join(prompt.split()[:3800])
 
-        try:
-            response = openai.Completion.create(
-                engine="gpt-3.5-turbo-instruct",
-                prompt=prompt,
-                max_tokens=200,
-                n=1,
-                stop=None
-            )
-            result = response.choices[0].text.lstrip()
-            return render_template('index.html', result=result)
-
-        except openai.error.OpenAIError as openai_error:
-            return render_template('error.html', message=f"OpenAI API Error: {openai_error}")
+       
+        response = ()
+        model = genai.GenerativeModel('gemini-pro')
+        response = model.generate_content(prompt)
+        print(response)
+        return response.text
 
     return render_template('index.html')
 
